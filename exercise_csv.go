3351eeb6e78eb2d6a00ceac0a727ff43707e657e
kiennguyenquoc <quockien.mmt06@gmail.com>
2016-03-24 10:02:45 +0700

    final

--- exercise_csv.go
+++ exercise_csv.go
@@ -1,12 +1,12 @@
 // CREATE TABLE userinfo
 // (
-//     ID INT PRIMARY KEY     NOT NULL,
+//     ID SERIAL PRIMARY KEY NOT NULL,
 //     first_name TEXT NOT NULL,
 //     last_name TEXT NOT NULL,
 //     company_name TEXT NOT NULL,
 //     address TEXT NOT NULL,
 //     city TEXT NOT NULL,
-//     county TEXT NOT NULL,
+//     province TEXT NOT NULL,
 //     postal TEXT NOT NULL,
 //     phone1 TEXT NOT NULL,
 //     phone2 TEXT NOT NULL,
@@ -14,12 +14,6 @@
 //     web TEXT NOT NULL
 // );
 
-// CREATE TABLE userinfo (
-//     uid INTEGER PRIMARY KEY AUTOINCREMENT,
-//     username VARCHAR(64) NULL,
-//     departname VARCHAR(64) NULL,
-//     created DATE NULL
-// );
 
 package main
 import (
@@ -40,47 +34,49 @@ const (
 
 func main() {
   fmt.Println("### - Read file CSV and import data to PostgreSQL")
-
+  fmt.Printf("StartTime: %v\n", time.Now())
+  // connect postgresql
   dbinfo := fmt.Sprintf("user=%s host=localhost password=%s dbname=%s sslmode=disable", DB_USER, DB_PASSWORD, DB_NAME)
   db, err := sql.Open("postgres", dbinfo)
-
   if err != nil {
     log.Fatal(err)
   }
 
-  fmt.Printf("StartTime: %v\n", time.Now())
-
-  csvFile, err := os.Open("data.csv")
-
+  drop_table, err := db.Exec("DROP TABLE userinfo")
   if err != nil {
     fmt.Println(err)
   }
+  fmt.Println(drop_table)
 
+  create_table, err := db.Exec("CREATE TABLE userinfo ( ID SERIAL PRIMARY KEY NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL, company_name TEXT NOT NULL, address TEXT NOT NULL, city TEXT NOT NULL, province TEXT NOT NULL, postal TEXT NOT NULL, phone1 TEXT NOT NULL, phone2 TEXT NOT NULL, email TEXT NOT NULL, web TEXT NOT NULL )" )
+  show_error(err)
+  fmt.Println(create_table)
+
+  csvFile, err := os.Open("data.csv")
+  show_error(err)
   defer csvFile.Close()
 
   reader := csv.NewReader(csvFile)
   reader.FieldsPerRecord = -1
   csvData, err := reader.ReadAll()
-
   if err != nil {
     fmt.Println(err)
     os.Exit(1)
   }
 
-  for _, each := range csvData {
-    // Thu hien luu xuong DB postgre
-    fmt.Println(each)
+  for _, row := range csvData {
+    // Insert data
+    _, err := db.Exec("INSERT INTO userinfo (first_name, last_name, company_name, address, city, province, postal, phone1, phone2, email, web) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)", row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
+    show_error(err)
   }
-
-  stmt, err := db.Exec("INSERT INTO userinfo(uid, username, departname, created) values(2, 'astaxie', '研发部门', '2012-12-09')")
-  if err != nil {
-    fmt.Println(err)
-  }
-  fmt.Println(stmt)
-
 
   db.Close()
   fmt.Printf("StopTime: %v\n", time.Now())
 }
 
+func show_error(err error) {
+  if err != nil {
+    fmt.Println(err)
+  }
+}
 
