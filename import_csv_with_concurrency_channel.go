b9d2b2cb17ac16c3ca8328f34d2aa0e25e57cf51
kiennguyenquoc <quockien.mmt06@gmail.com>
2016-03-29 17:32:12 +0700

    fix conslict

--- import_csv_with_concurrency_channel.go
+++ import_csv_with_concurrency_channel.go
@@ -1,12 +1,12 @@
 package main
 import (
-  "fmt"
-  "time"
-  "os"
-  "encoding/csv"
-  "database/sql"
-  _ "github.com/lib/pq"
-  "sync"
+"fmt"
+"time"
+"os"
+"encoding/csv"
+"database/sql"
+_ "github.com/lib/pq"
+"sync"
 )
 
 const (
@@ -23,11 +23,11 @@ func main() {
   db, err := sql.Open("postgres", dbinfo)
   show_error(err)
 
-  drop_table, err := db.Exec("DROP TABLE IF EXISTS userinfo_channel")
+  drop_table, err := db.Exec("DROP TABLE userinfo_channel")
   show_error(err)
   fmt.Println(drop_table)
 
-  create_table, err := db.Exec("CREATE TABLE IF NOT EXISTS userinfo_channel ( ID SERIAL PRIMARY KEY NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL, company_name TEXT NOT NULL, address TEXT NOT NULL, city TEXT NOT NULL, province TEXT NOT NULL, postal TEXT NOT NULL, phone1 TEXT NOT NULL, phone2 TEXT NOT NULL, email TEXT NOT NULL, web TEXT NOT NULL )" )
+  create_table, err := db.Exec("CREATE TABLE userinfo_channel ( ID SERIAL PRIMARY KEY NOT NULL, first_name TEXT NOT NULL, last_name TEXT NOT NULL, company_name TEXT NOT NULL, address TEXT NOT NULL, city TEXT NOT NULL, province TEXT NOT NULL, postal TEXT NOT NULL, phone1 TEXT NOT NULL, phone2 TEXT NOT NULL, email TEXT NOT NULL, web TEXT NOT NULL )" )
   show_error(err)
   fmt.Println(create_table)
   defer db.Close()
@@ -71,9 +71,15 @@ func get_data_from_csv(file_name string, c chan [][]string){
 
 func import_data_csv(data [][]string, db *sql.DB, wg *sync.WaitGroup) {
   defer wg.Done()
-  var querry string
+
+  insertData, err := db.Prepare("INSERT INTO userinfo_channel (first_name, last_name, company_name, address, city, province, postal, phone1, phone2, email, web) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
+  show_error(err)
+  defer insertData.Close()
+
+  tx, err := db.Begin()
   for _, row := range data {
-    _, err := db.Exec("INSERT INTO userinfo_channel (first_name, last_name, company_name, address, city, province, postal, phone1, phone2, email, web) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)", row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
+    _, err := tx.Stmt(insertData).Exec(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10])
     show_error(err)
   }
+  tx.Commit()
 }
